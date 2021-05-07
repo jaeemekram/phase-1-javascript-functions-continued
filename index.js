8e2512a1a8b42e14cd0953d9ad60a65c262b87db
Jaeem Ekram <jekram98@gmail.com>
2021-05-07 17:45:00 -0400

    commit tree 034cc5aa4d5f4c9f649f3ae0070b5b6206ea98ec

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function saturdayFun(var1= "roller-skate") {
+    return(`This Saturday, I want to ${var1}!`);
+}
+
+
+
+const mondayWork = function(var2= "go to the office") {
+    return(`This Monday, I will ${var2}.`)
+}
+
+function wrapAdjective(string= "*") {
+    return function (var3= "special") {
+        return(`You are ${string}${var3}${string}!`)
+    }
+}
+
+// function wrapAdjective(flair = '*'){
+//     return function(string = 'special'){
+//         return `You are ${flair}${string}${flair}!`
+//     }
+// }
